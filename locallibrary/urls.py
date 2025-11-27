63c94e5f078f7de36fffb753734b3be31e53d9ff
Bogdan-Taran <taranbogdan9.2@mail.ru>
2025-11-27 13:43:54 +0700

    Pages login, logout and authentication were made

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,3 +13,8 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)    #включение размещения статических файлов
+
+# аутентификация
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
